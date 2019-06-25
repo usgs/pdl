fe1f99ebb661f1d27796846ecb3cc24ebbd03119
Bass <cbass@igskcicgwsgm015.gs.doi.net>
2019-06-25 09:13:12 -0600

    Added framework for new extent listener

--- src/main/java/gov/usgs/earthquake/indexer/ExtentIndexerListener.java
+++ src/main/java/gov/usgs/earthquake/indexer/ExtentIndexerListener.java
@@ -4,6 +4,37 @@
 
 package gov.usgs.earthquake.indexer;
 
- public class ExtentIndexerListener extends DefaultIndexerListener implements IndexerListener {
+import gov.usgs.util.Config;
+
+public class ExtentIndexerListener extends DefaultIndexerListener implements IndexerListener {
+
+  //Constructor
+  //Just uses super configure for now
+  public ExtentIndexerListener() {
+    super();
+  }
+
+  //Sets up object on start
+  //Will set up thread and then tell it to chill (probably)
+  //For now just using super configure
+  //public void configure(Config config) throws Exception {  }
+
+  //Called when indexer does something
+  //Will wake our thread up as long as we're not busy processing still
+  public void onIndexerEvent(IndexerEvent delta) throws Exception {
+
+  }
+
+  //Max number of tries indexer gives listener to process
+  //(For if listener tosses exception during processing)
+	public int getMaxTries() {
+    return 0;
+  }
+
+  //Number of milliseconds given by indexer before timing out
+  //Not going to be super long because we will be using a thread
+	public long getTimeout() {
+    return 0;
+  }
    
  }
