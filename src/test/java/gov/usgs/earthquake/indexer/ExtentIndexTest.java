f27c0a5fd677727f53c89c293aedff2ff386134e
Capprin Bass <cbass@usgs.gov>
2019-07-11 15:46:41 -0600

    Made changes to satisfy PR Review

--- src/test/java/gov/usgs/earthquake/indexer/ExtentIndexTest.java
+++ src/test/java/gov/usgs/earthquake/indexer/ExtentIndexTest.java
@@ -5,8 +5,6 @@
 package gov.usgs.earthquake.indexer;
 
 import junit.framework.Assert;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import gov.usgs.util.Config;
@@ -19,23 +17,14 @@ public class ExtentIndexTest {
 
   private ExtentIndex index;
 
-  @Before
-  public void setup() throws Exception{
-    index = new ExtentIndex();
-		index.configure(new Config());
-		index.startup();
-  }
-
-  @After
-  public void shutdown() throws Exception{
-    index.shutdown();
-  }
-
   @Test
   public void addExistsTest() throws Exception{
-    long testIndex = 9;
+    index = new ExtentIndex();
+    index.configure(new Config());
+    index.startup();
 
     //create product
+    long testIndex = 9;
     ExtentSummary product = new ExtentSummary();
     product.setId(testIndex);
     product.setMaxLatitude(0.); //Add at least one parameter to pass validity check
@@ -52,5 +41,7 @@ public class ExtentIndexTest {
     Connection conn = index.connect();
     PreparedStatement stmnt = conn.prepareStatement("DELETE FROM " + ExtentIndex.EXTENT_TABLE + " WHERE " + ExtentIndex.EXTENT_INDEX_ID + "=" + product.getId());
     stmnt.executeUpdate();
+
+    index.shutdown();
   }
 }
