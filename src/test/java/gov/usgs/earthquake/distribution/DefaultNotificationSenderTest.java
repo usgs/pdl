9f889d1705cb21a8497f07a1582266061e336736
Capprin Bass <cbass@usgs.gov>
2019-08-08 13:52:18 -0600

    Added additional test for DefaultNotificationSender

--- src/test/java/gov/usgs/earthquake/distribution/DefaultNotificationSenderTest.java
+++ src/test/java/gov/usgs/earthquake/distribution/DefaultNotificationSenderTest.java
@@ -2,37 +2,59 @@ package gov.usgs.earthquake.distribution;
 
 import gov.usgs.earthquake.product.Product;
 import gov.usgs.earthquake.product.ProductId;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
 import java.net.URL;
+import java.util.Date;
 import java.util.logging.Logger;
 
 public class DefaultNotificationSenderTest {
 
   private String sentMessage;
-  private Object syncObject = new Object();
+  URLProductStorage storage;
+  DefaultNotificationSender notificationSender;
+  ProductId testId;
+  Product testProduct;
 
-  @Test
-  public void onProductTest() throws Exception {
-    URLProductStorage storage = new URLProductStorage(new File("storage"),new URL("http://localhost/"));
+  @Before
+  public void setup() throws Exception{
+    storage = new URLProductStorage(new File("storage"),new URL("http://localhost/"));
 
-    DefaultNotificationSender notificationSender = new TestDefaultNotificationSender();
+    notificationSender = new TestDefaultNotificationSender();
     notificationSender.setProductStorage(storage);
     notificationSender.setProductStorageMaxAge(60000); // one minute
     notificationSender.setNotificationIndex(new JDBCNotificationIndex());
 
-    ProductId id = new ProductId("testSource","testType","testCode");
-    Product product = new Product(id);
-    product.setTrackerURL(new URL("http://localhost/"));
+    testId = new ProductId("testSource","testType","testCode");
+    testProduct = new Product(testId);
+    testProduct.setTrackerURL(new URL("http://localhost/"));
+  }
 
-    notificationSender.onProduct(product);
+  @Test
+  public void onExpiredNotificationTest() throws Exception {
+    storage.storeProduct(testProduct);
+    URLNotification testNotification = new URLNotification(
+            testId,
+            new Date(new Date().getTime() + 60000),
+            testProduct.getTrackerURL(),
+            storage.getProductURL(testId));
+    notificationSender.onExpiredNotification(testNotification);
 
-    Assert.assertEquals(id.toString(),sentMessage);
+    Assert.assertNull(storage.getProduct(testId));
+  }
+
+  @Test
+  public void onProductTest() throws Exception {
+    notificationSender.onProduct(testProduct);
+
+    Assert.assertEquals(testId.toString(),sentMessage);
 
     // clean up
-    storage.removeProduct(id);
+    storage.removeProduct(testId);
   }
 
   private class TestDefaultNotificationSender extends DefaultNotificationSender {
