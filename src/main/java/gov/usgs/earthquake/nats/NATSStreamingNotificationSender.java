4043b36dbaa89d69928cd90e727a7d486b7b0801
Capprin Bass <cbass@usgs.gov>
2019-08-09 15:50:54 -0600

    Wrote first pass of NATSStreamingNotificationSender

--- src/main/java/gov/usgs/earthquake/nats/NATSStreamingNotificationSender.java
+++ src/main/java/gov/usgs/earthquake/nats/NATSStreamingNotificationSender.java
@@ -1,30 +1,60 @@
 package gov.usgs.earthquake.nats;
 
-import gov.usgs.earthquake.distribution.DefaultNotificationSender;
-import gov.usgs.earthquake.distribution.Notification;
+import gov.usgs.earthquake.distribution.*;
+import gov.usgs.util.Config;
 import io.nats.streaming.StreamingConnection;
 import io.nats.streaming.StreamingConnectionFactory;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
+import java.util.logging.Logger;
 
 public class NATSStreamingNotificationSender extends DefaultNotificationSender {
+
+  private static final Logger LOGGER = Logger
+          .getLogger(DefaultNotificationSender.class.getName());
+
+  public static String CLUSTER_ID_PROPERTY = "clusterid";
+  public static String CLIENT_ID_PROPERTY = "clientid";
+  public static String SUBJECT_PROPERTY = "subject";
 
   private String clusterId;
   private String clientId;
   private String subject;
   private StreamingConnection stanConnection;
 
+  public void configure(Config config) throws Exception{
+    super.configure(config);
+
+    clusterId = config.getProperty(CLUSTER_ID_PROPERTY);
+    if (clusterId == null) {
+      throw new ConfigurationException("[" + getName() + "] " + CLUSTER_ID_PROPERTY + " must be defined");
+    }
+    clientId = config.getProperty(CLIENT_ID_PROPERTY);
+    if (clientId == null) {
+      throw new ConfigurationException("[" + getName() + "] " + CLIENT_ID_PROPERTY + " must be defined");
+    }
+    subject = config.getProperty(SUBJECT_PROPERTY);
+    if (subject == null) {
+      throw new ConfigurationException("[" + getName() + "] " + SUBJECT_PROPERTY + " must be defined");
+    }
+  }
+
   @Override
-  public void sendMessage(String message) {
+  public void sendMessage(String message) throws Exception{
     try {
       stanConnection.publish(subject, message.getBytes());
     } catch (TimeoutException te) {
       // if we lose connection to server
+      LOGGER.warning("[" + getName() + "] NATS Streaming server inaccessible. Can't publish notification " + message);
+      throw te;
     } catch (InterruptedException ie) {
       // if we're interrupted when sending
+      LOGGER.warning("[" + getName() + "] Interrupted while sending notification " + message);
+      throw ie;
     } catch (IOException ioe) {
       // if there's a problem with the message
+      throw ioe;
     }
   }
 
@@ -32,7 +62,7 @@ public class NATSStreamingNotificationSender extends DefaultNotificationSender {
   // everywhere
   @Override
   public String notificationToString(Notification notification) throws Exception{
-    return super.notificationToString(notification);
+    return URLNotificationJSONConverter.toJSON((URLNotification) notification);
   }
 
   public void startup() throws Exception {
