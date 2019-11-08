1a0715d9a6476004ecf8557e3a980fd2c91bbf51
Jeremy Fee <jmfee@usgs.gov>
2019-11-08 10:49:15 -0700

    Fix javadoc bug

--- src/main/java/gov/usgs/earthquake/indexer/ExtentIndexerListener.java
+++ src/main/java/gov/usgs/earthquake/indexer/ExtentIndexerListener.java
@@ -7,11 +7,12 @@ import gov.usgs.util.Config;
 import gov.usgs.earthquake.distribution.ConfigurationException;
 
 /**
- * ExtentIndexerListener is an extension of the ReliableIndexerListener. It populates the ExtentSummary table with viable products.
- * 
+ * ExtentIndexerListener is an extension of the ReliableIndexerListener. It
+ * populates the ExtentSummary table with viable products.
+ *
  * This listener takes an ExtentIndex for querying and table updates:
  * <dl>
- * <dt>index</index>
+ * <dt>index</dt>
  * <dd>(Required) the ExtentIndex used for querying and updates</dd>
  * </dl>
  */
@@ -19,16 +20,17 @@ public class ExtentIndexerListener extends ReliableIndexerListener {
 
   /**
    * Configures listener, checking for correct type
-   * 
+   *
    * @param config configuration
-   * 
+   *
    * @throws ConfigurationException if incorrect type provided
    */
   @Override
   public void configure(Config config) throws Exception {
     super.configure(config);
     if (!(this.getProductIndex() instanceof ExtentIndex)) {
-      throw new ConfigurationException("[" + getName() + "] index must be of type ExtentIndex. Given type " + productIndex.getClass().getName());
+      throw new ConfigurationException(
+          "[" + getName() + "] index must be of type ExtentIndex. Given type " + productIndex.getClass().getName());
     }
   }
 
@@ -39,24 +41,23 @@ public class ExtentIndexerListener extends ReliableIndexerListener {
    */
   @Override
   protected void onBeforeProcessThreadStart() throws Exception {
-    setLastIndexId(((ExtentIndex)productIndex).getLastExtentIndexId());
+    setLastIndexId(((ExtentIndex) productIndex).getLastExtentIndexId());
   }
 
   /**
    * Hands product to index to be added to table
-   * 
+   *
    * @param product the product to be added
    *
    * @throws Exception if ExtentIndex can't do database transaction
    */
   @Override
-  public void processProduct(ProductSummary product) throws Exception{
+  public void processProduct(ProductSummary product) throws Exception {
     super.processProduct(product);
     ExtentSummary extent = new ExtentSummary(product);
 
-    ((ExtentIndex)productIndex).addExtentSummary(extent);
+    ((ExtentIndex) productIndex).addExtentSummary(extent);
     setLastIndexId(product.getIndexId());
   }
 
-  
 }
