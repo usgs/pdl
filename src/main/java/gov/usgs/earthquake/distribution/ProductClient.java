c9d0d49f444ac2f40cf9dacab31b02180b5e7de6
Jeremy Fee <jmfee@usgs.gov>
2019-11-08 10:37:30 -0700

    v2.2.0

--- src/main/java/gov/usgs/earthquake/distribution/ProductClient.java
+++ src/main/java/gov/usgs/earthquake/distribution/ProductClient.java
@@ -64,7 +64,7 @@ public class ProductClient extends DefaultConfigurable implements
 		ProductClientMBean, Bootstrappable {
 
 	/** The "release" version number. */
-	public static final String RELEASE_VERSION = "Version 2.1.1 2019-05-28";
+	public static final String RELEASE_VERSION = "Version 2.2.0 2019-11-08";
 
 	/** Property name used on products for current RELEASE_VERSION. */
 	public static final String PDL_CLIENT_VERSION_PROPERTY = "pdl-client-version";
