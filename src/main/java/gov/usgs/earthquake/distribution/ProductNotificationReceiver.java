00f8c16fb2f210d0cea7676f5e13f68a6b2d6606
Capprin Bass <cbass@usgs.gov>
2019-12-09 15:30:29 -0700

    Added skeleton for ProductNotificationReceiver class

---
+++ src/main/java/gov/usgs/earthquake/distribution/ProductNotificationReceiver.java
@@ -0,0 +1,18 @@
+package gov.usgs.earthquake.distribution;
+
+import gov.usgs.util.Config;
+
+public class ProductNotificationReceiver extends DefaultNotificationReceiver {
+
+  public void configure(Config config) {
+
+  }
+
+  public void startup() {
+
+  }
+
+  public void shutdown() {
+
+  }
+}
