f76640e2c0dce7da9b704954b48cdb5f01956c1e
Capprin Bass <cbass@usgs.gov>
2020-01-22 16:57:59 -0700

    Added structure for ProductNotificationReceiver (tentative)

--- src/main/java/gov/usgs/earthquake/distribution/ProductNotificationReceiver.java
+++ src/main/java/gov/usgs/earthquake/distribution/ProductNotificationReceiver.java
@@ -2,17 +2,42 @@ package gov.usgs.earthquake.distribution;
 
 import gov.usgs.util.Config;
 
+/**
+ * Receives notifications from an arbitrary web service.
+ */
 public class ProductNotificationReceiver extends DefaultNotificationReceiver {
 
-  public void configure(Config config) {
+  public static final String SERVER_HOST_PROPERTY = "serverHost";
+  public static final String SERVER_PORT_PROPERTY = "serverPort";
 
+  public static final String DEFAULT_SERVER_HOST = "http://www.google.com";
+  public static final String DEFAULT_SERVER_PORT = "4222";
+
+  private String serverHost;
+  private String serverPort;
+
+  @Override
+  public void configure(Config config) throws Exception {
+    super.configure(config);
+
+    String serverHost = config.getProperty(SERVER_HOST_PROPERTY, DEFAULT_SERVER_HOST);
+    String serverPort = config.getProperty(SERVER_PORT_PROPERTY, DEFAULT_SERVER_PORT);
   }
 
+  @Override
   public void startup() {
+    //open websocket
+    //  - need to write a @ClientEndpoint websocketclient that does message handling
+    //    - default messaging, open, close behavior
+    //    - how pings work
 
+    //add DefaultNotificationReceiver's receiveNotification (maybe wrapped) as a listener
+
+    //done (I think)
   }
 
+  @Override
   public void shutdown() {
-
+    //close socket
   }
 }
