e0d7e9c5e870e484cf446517bd4b9e36cdbc2558
Capprin Bass <cbass@usgs.gov>
2020-02-10 14:56:27 -0700

    Added environment configuration and refactored variables

--- etc/node_ws/app.js
+++ etc/node_ws/app.js
@@ -1,16 +1,20 @@
-// these parameters should be configurable
-const host_name = '127.0.0.1';
-const port_name = 8080;
-const sub_path = "/subscribe/"
-const cluster_id = 'usgs'
-const channel = 'anss.pdl.realtime'
+// Defaults; configurable through environment
+const HOST = process.env.HOST || '127.0.0.1';
+const PORT = process.env.PORT || 8080;
+const SUB_PATH = process.env.SUB_PATH || "/subscribe/"
+const CLUSTER_ID = process.env.CLUSTER_ID || 'usgs'
+const CHANNEL = process.env.CHANNEL || 'anss.pdl.realtime'
+const PING_INTERVAL = process.env.PING_INTERVAL || 30000;
+
+var package = require('./package.json')
+console.log("Starting " + package.name + " version " + package.version);
 
 // stan setup
 const stan = require('node-nats-streaming');
 
 // web socket setup
 const WebSocket = require('ws');
-const server = new WebSocket.Server({host: host_name, port:port_name});
+const server = new WebSocket.Server({host:HOST, port:PORT});
 
 // uuid
 const { uuid } = require('uuidv4');
@@ -19,30 +23,31 @@ console.log('Setup complete, waiting for connections...');
 
 server.on('connection', function onConnection(ws, req) {
   console.log('Connection opened with URL ' + req.url);
-  this.isAlive = true;
+  ws.isAlive = true;
 
-  // store stan connection
+  // store socket-specific parameters (should be member vars?)
   var conn;
-
   var id = uuid();
 
   // get sequence
-  var seq_pos = req.url.lastIndexOf(sub_path);
+  var seq_pos = req.url.lastIndexOf(SUB_PATH);
   if (seq_pos == -1) {
     console.log('Sequence path does not exist')
     ws.close(); //also do malformed URL?
+    return;
   }
-  seq_str = req.url.slice(seq_pos + sub_path.length);
+  seq_str = req.url.slice(seq_pos + SUB_PATH.length);
   var seq = parseInt(seq_str);
   if (seq == NaN) {
     console.log('Sequence ' + seq_str + ' is not an integer');
     ws.close(); //also do malformed URL?
+    return;
   }
 
   console.log('Provided sequence: ' + seq_str);
 
   // do stan connection
-  conn = stan.connect(cluster_id, id); //need custom client id's for each ws connection; will use better behavior eventually.
+  conn = stan.connect(CLUSTER_ID, id);
 
   console.log('Connecting to stan server with id: ' + id);
 
@@ -53,9 +58,9 @@ server.on('connection', function onConnection(ws, req) {
     var opts = conn.subscriptionOptions();
     opts.setStartAtSequence(seq);
     // subscribe to channel
-    var subscription = conn.subscribe(channel, opts);
+    var subscription = conn.subscribe(CHANNEL, opts);
 
-    console.log('Subscribed to channel: ' + channel);
+    console.log('Subscribed to channel: ' + CHANNEL);
 
     // define stan message behavior
     subscription.on('message', function onStanMessage(message) {
@@ -71,7 +76,6 @@ server.on('connection', function onConnection(ws, req) {
     // close stan connection
     conn.close();
     console.log('Connection closed.');
-
   });
 
   ws.on('message', function onMessage() {
@@ -79,18 +83,16 @@ server.on('connection', function onConnection(ws, req) {
   });
 
   ws.on('pong', function heartbeat() {
-    //console.log('Received pong');
     this.isAlive = true;
   })
 });
 
 // periodic pings
 const interval = setInterval(function pingInterval() {
-  //console.log('Sending ping');
   server.clients.forEach(function(ws) {
     if (ws.isAlive === false) return ws.terminate();
 
     ws.isAlive = false;
     ws.ping();
   });
-}, 30000); //ping interval should be configurable
+}, PING_INTERVAL);
