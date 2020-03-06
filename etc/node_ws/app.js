3182bcf3a977e792f0a264d8ffaa199fa258d227
Capprin Bass <cbass@usgs.gov>
2020-03-06 14:20:58 -0700

    Updated node server with pr changes

--- etc/node_ws/app.js
+++ etc/node_ws/app.js
@@ -10,55 +10,52 @@ class Handler {
     this.ip = request.connection.remoteAddress;
     this.socket = socket;
 
-    let seq = this.getSequence(request.url);
-    if (seq == null) {
-      console.log('Sequence not provided by ' + this.ip + ' or is not an integer; closing connection.');
-      this.socket.close(1008, 'Malformed/nonexistent sequence');
-    }
-    this.sequence = seq;
+    this.sequence = this.getSequence(request.url);
   }
+
   start() {
     console.log('Connection opened from IP: ' + this.ip);
 
+    if (this.sequence == null) {
+      console.log('Sequence not provided by ' + this.ip + ' or is not an integer; closing connection.');
+      this.socket.close(1008, 'Malformed/nonexistent sequence');
+      return;
+    }
+
     // connect to nats streaming
     console.log('Connecting to stan server for client ' + this.ip + ' with id ' + this.id);
-    let connection = stan.connect(this.config.cluster_id, this.id);
-    connection.on('connect', (connection) => this.onStanConnection(connection));
+    const connection = stan.connect(this.config.cluster_id, this.id);
+    connection.on('connect', (connection) => {
+      console.log('Connected to stan server with id ' + this.id);
+      let opts = connection.subscriptionOptions();
+      opts.setStartAtSequence(this.sequence);
+      let subscription = connection.subscribe(this.config.channel, opts);
+      console.log('Client with IP ' + this.ip + ' subscribed to channel ' + this.config.channel);
+      subscription.on('message', (message) => this.onStanMessage(message));
+    });
 
-    // define socket behavior
-    this.socket.isAlive = true;
+    // define socket close behavior
     this.socket.on('close', () => {
       connection.close();
       console.log('Connection closed for client ' + this.ip);
     });
-    this.socket.on('pong', () => {
-      this.socket.isAlive = true;
-      if (this.config.log_level == 'all') console.log('Received pong from client with ip ' + this.ip);
-    });
   }
 
-  onStanConnection(connection) {
-    console.log('Connected to stan server with id ' + this.id);
-    let opts = connection.subscriptionOptions();
-    opts.setStartAtSequence(this.sequence);
-    let subscription = connection.subscribe(this.config.channel, opts);
-
-    console.log('Client with IP ' + this.ip + ' subscribed to channel ' + this.config.channel);
-
-    subscription.on('message', (message) => {
-      let json = {sequence: message.getSequence(), timestamp: message.getTimestampRaw(), data: JSON.parse(message.getData())};
-      this.socket.send(JSON.stringify(json));
-      if (this.config.log_level == 'all') console.log('Received STAN notification. Payload: ' + JSON.stringify(json));
-    });
+  onStanMessage(message) {
+    const json = {sequence: message.getSequence(), timestamp: message.getTimestampRaw(), data: JSON.parse(message.getData())};
+    this.socket.send(JSON.stringify(json));
+    if (this.config.log_level == 'all') {
+      console.log('Received STAN notification. Payload: ' + JSON.stringify(json));
+    }
   }
 
   getSequence(url) {
-    let seq_pos = url.lastIndexOf(this.config.subscribe_path);
+    const seq_pos = url.lastIndexOf(this.config.subscribe_path);
     if (seq_pos == -1) {
       return null;
     }
-    let seq_str = url.slice(seq_pos + this.config.subscribe_path.length);
-    let seq = parseInt(seq_str);
+    const seq_str = url.slice(seq_pos + this.config.subscribe_path.length);
+    const seq = parseInt(seq_str);
     if (seq == NaN) {
       return null;
     }
@@ -69,21 +66,31 @@ class Handler {
 class App {
   constructor(config) {
     this.config = config;
-    this.server = new WebSocket.Server({host:this.config.host, port:this.config.port});    
   }
   
   start() {
-    this.server.on('connection', (socket, request) => {
-      new Handler(this.config, socket, request).start();
-    });
+    this.server = new WebSocket.Server({host:this.config.host, port:this.config.port});
+    this.server.on('connection', (socket, request) => this.onConnection(socket,request));
     setInterval(() => this.ping(), this.config.ping_interval);
     console.log('Web socket hosted at ' + this.config.host + ':' + this.config.port + '.');
   }
 
+  onConnection(socket, request) {
+    socket.isAlive = true;
+    socket.on('pong', () => {
+      socket.isAlive = true;
+    });
+    new Handler(this.config, socket, request).start();
+  }
+
   ping() {
-    if (this.config.log_level == 'all') console.log('Pinging clients');
+    if (this.config.log_level == 'all') {
+      console.log('Pinging clients');
+    }
     this.server.clients.forEach((ws) => {
-      if (ws.isAlive === false) return ws.close(1008, 'Stale connection');
+      if (ws.isAlive === false) {
+        return ws.close(1008, 'Stale connection');
+      }
       ws.isAlive = false;
       ws.ping();
     });
