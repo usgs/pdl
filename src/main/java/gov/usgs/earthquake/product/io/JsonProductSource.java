a06f5068136b1781ca21b16bb0c4bbcf26262d0b
Jeremy Fee <jmfee@usgs.gov>
2020-10-13 08:56:32 -0600

    Update json product format, aws product sender, add json to url product storage

---
+++ src/main/java/gov/usgs/earthquake/product/io/JsonProductSource.java
@@ -0,0 +1,58 @@
+/*
+ * JsonProductSource
+ */
+package gov.usgs.earthquake.product.io;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import javax.json.Json;
+import javax.json.JsonReader;
+
+import gov.usgs.util.StreamUtils;
+
+import gov.usgs.earthquake.product.Product;
+
+
+/**
+ * Load a product from an InputStream containing Json.
+ */
+public class JsonProductSource implements ProductSource {
+
+	/** The input stream where Json is read. */
+	private InputStream in;
+
+	/**
+	 * Create a new JsonProductSource.
+	 *
+	 * @param in
+	 *            the input stream where Json is read.
+	 */
+	public JsonProductSource(final InputStream in) {
+		this.in = in;
+	}
+
+	/**
+	 * Begin reading the input stream, sending events to out.
+	 *
+	 * @param out
+	 *            the receiving ProductOutput.
+	 */
+	public synchronized void streamTo(ProductHandler out) throws Exception {
+    final Product product;
+    try (final JsonReader reader = Json.createReader(new InputStreamReader(in))) {
+      product = new JsonProduct().getProduct(reader.readObject());
+    }
+    final ObjectProductSource source = new ObjectProductSource(product);
+    source.streamTo(out);
+	}
+
+	/**
+	 * Free any resources associated with this handler.
+	 */
+	@Override
+	public void close() {
+		StreamUtils.closeStream(in);
+	}
+
+}
