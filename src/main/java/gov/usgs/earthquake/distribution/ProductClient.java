d0b4c752e6d3e62ed5a0b78bb21e4f6227c8dab3
Jeremy Fee <jmfee@usgs.gov>
2020-10-27 13:50:56 -0600

    Update release version for rc

--- src/main/java/gov/usgs/earthquake/distribution/ProductClient.java
+++ src/main/java/gov/usgs/earthquake/distribution/ProductClient.java
@@ -64,7 +64,7 @@ public class ProductClient extends DefaultConfigurable implements
 		ProductClientMBean, Bootstrappable {
 
 	/** The "release" version number. */
-	public static final String RELEASE_VERSION = "Version 2.5.2 2020-07-09";
+	public static final String RELEASE_VERSION = "Version 2.6.0-rc1 2020-10-27";
 
 	/** Property name used on products for current RELEASE_VERSION. */
 	public static final String PDL_CLIENT_VERSION_PROPERTY = "pdl-client-version";
