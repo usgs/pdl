26fbc6c6dcbad6c478eecc5834be062324c51d20
Jeremy Fee <jmfee@usgs.gov>
2020-11-04 17:05:50 -0700

    Update config logging, add missing insert

--- src/main/java/gov/usgs/earthquake/aws/JsonProductStorage.java
+++ src/main/java/gov/usgs/earthquake/aws/JsonProductStorage.java
@@ -63,7 +63,7 @@ public class JsonProductStorage extends JDBCConnection implements ProductStorage
     driver = config.getProperty("driver", DEFAULT_DRIVER);
     LOGGER.config("[" + getName() + "] driver=" + driver);
     table = config.getProperty("table", DEFAULT_TABLE);
-    LOGGER.config("[" + getName() + "] table=" + driver);
+    LOGGER.config("[" + getName() + "] table=" + table);
     url = config.getProperty("url", DEFAULT_URL);
     // do not log url, it may contain user/pass
   }
