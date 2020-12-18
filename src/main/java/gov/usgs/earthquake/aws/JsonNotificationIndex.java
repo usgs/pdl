3d6b4785bc07e59f00e5389aafe1f546e5df7aa7
Jeremy Fee <jmfee@usgs.gov>
2020-12-18 13:32:03 -0700

    Remove unused imports, fix warnings

--- src/main/java/gov/usgs/earthquake/aws/JsonNotificationIndex.java
+++ src/main/java/gov/usgs/earthquake/aws/JsonNotificationIndex.java
@@ -2,8 +2,6 @@ package gov.usgs.earthquake.aws;
 
 import java.io.ByteArrayInputStream;
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
--- src/main/java/gov/usgs/earthquake/aws/TrackingIndex.java
+++ src/main/java/gov/usgs/earthquake/aws/TrackingIndex.java
@@ -1,8 +1,6 @@
 package gov.usgs.earthquake.aws;
 
 import java.io.ByteArrayInputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
