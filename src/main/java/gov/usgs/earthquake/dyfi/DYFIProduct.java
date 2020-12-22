12dc9f04432ba38fea05145e3313ff1081271ad2
Jeremy Fee <jmfee@usgs.gov>
2020-12-21 19:32:44 -0700

    Skip downloading dyfi event.xml when properties already set

--- src/main/java/gov/usgs/earthquake/dyfi/DYFIProduct.java
+++ src/main/java/gov/usgs/earthquake/dyfi/DYFIProduct.java
@@ -2,7 +2,6 @@ package gov.usgs.earthquake.dyfi;
 
 import gov.usgs.earthquake.product.Content;
 import gov.usgs.earthquake.product.Product;
-import gov.usgs.util.StreamUtils;
 
 import java.io.InputStream;
 import java.math.BigDecimal;
@@ -32,30 +31,34 @@ public class DYFIProduct extends Product {
 	 * basis. The given product must have a
 	 * <code>DYFI_EVENT_XML_ATTACHMENT</code> in order to successfully create a
 	 * DYFIProduct.
-	 * 
+	 *
 	 * @param product
 	 *            The product serving as a basis for this instance.
 	 */
 	public DYFIProduct(final Product product) {
 		super(product);
 
+		if (product.getProperties().get(DYFI_NUM_RESP_PROPERTY) == null
+				|| product.getProperties().get(DYFI_MAX_MMI_PROPERTY) == null) {
+			// only load xml if properties not set
+			loadEventXml();
+		}
+	}
+
+	protected void loadEventXml() {
 		// Parse event_data.xml for more information about product
-		Content source = product.getContents().get(DYFI_EVENT_XML_ATTACHMENT);
-		if (source != null) {
-			EventDataXMLHandler handler = new EventDataXMLHandler(this);
-			InputStream in = null;
-			try {
-				in = source.getInputStream();
-				handler.parse(in);
-			} catch (Exception e) {
-				throw new IllegalArgumentException(e);
-			} finally {
-				StreamUtils.closeStream(in);
-			}
-		} else {
+		Content source = getContents().get(DYFI_EVENT_XML_ATTACHMENT);
+		if (source == null) {
 			throw new IllegalArgumentException("Given product lacked the "
 					+ "required file to be a DYFIProduct. ("
 					+ DYFI_EVENT_XML_ATTACHMENT + ")");
+		}
+
+		EventDataXMLHandler handler = new EventDataXMLHandler(this);
+		try (final InputStream in = source.getInputStream()) {
+			handler.parse(in);
+		} catch (Exception e) {
+			throw new IllegalArgumentException(e);
 		}
 	}
 
@@ -68,7 +71,7 @@ public class DYFIProduct extends Product {
 
 	/**
 	 * Set the number of felt reports submitted for this event.
-	 * 
+	 *
 	 * @param numResponses
 	 *            The new number of submitted felt reports.
 	 * @throws NumberFormatException
@@ -93,7 +96,7 @@ public class DYFIProduct extends Product {
 
 	/**
 	 * Sets the maximum reported intensity for this event.
-	 * 
+	 *
 	 * @param maxMMI
 	 *            The new maximum reported intensity.
 	 * @throws NumberFormatException
@@ -105,6 +108,6 @@ public class DYFIProduct extends Product {
 		// at least makes sense; an exception is thrown for garbage input.
 		Double.parseDouble(maxMMI);
 
-		getProperties().put("maxmmi", maxMMI);
+		getProperties().put(DYFI_MAX_MMI_PROPERTY, maxMMI);
 	}
 }
