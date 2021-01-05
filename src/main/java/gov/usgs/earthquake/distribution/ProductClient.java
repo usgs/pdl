b503199bacf423cc053cfdc941f1762df7830677
Jeremy Fee <jmfee@usgs.gov>
2021-01-05 16:58:07 -0700

    Update version to 2.7.6

--- src/main/java/gov/usgs/earthquake/distribution/ProductClient.java
+++ src/main/java/gov/usgs/earthquake/distribution/ProductClient.java
@@ -64,7 +64,7 @@ public class ProductClient extends DefaultConfigurable implements
 		ProductClientMBean, Bootstrappable {
 
 	/** The "release" version number. */
-	public static final String RELEASE_VERSION = "Version 2.7.5 2021-01-04";
+	public static final String RELEASE_VERSION = "Version 2.7.6 2021-01-05";
 
 	/** Property name used on products for current RELEASE_VERSION. */
 	public static final String PDL_CLIENT_VERSION_PROPERTY = "pdl-client-version";
